dd5e5452fc1564d25971dad43ea09d5963b5b9f5
Connor Morrissy <74273978+cmorrissy7@users.noreply.github.com>
2022-05-10 13:43:55 -0500

    Delete Exercise2.2.py

--- Exercise2.2.py
+++
@@ -1,8 +0,0 @@
-# Get the projected total sales from user
-total_sales = float(input('Enter the projected amount of total sales: '))
-
-# Calculate profit by mulitplying total sales by 23 percent
-profit = total_sales * .23
-
-# Display profit
-print('Projected profit: ', profit)
